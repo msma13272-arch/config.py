6f60c018fc8de1ac236a262e5bfcd4dbccd10c42
msma13272-arch <msma13272@gmail.com>
2026-05-22 12:33:55 +0600

    Update and rename README.md to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8300800052:AAHIW49zZS4luHxEkMDZvjQnJ0KbKI9yY3U")
+YOUTUBE_API_KEY = os.getenv("AIzaSyDvrz8mryvtDCt44sHrmyJ3i0zob59eX-I")
